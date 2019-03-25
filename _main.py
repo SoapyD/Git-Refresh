658998f79205cd023281634ff322f7ce88d0df08
SoapyD <tom_bombchild@hotmail.com>
2019-03-25 10:21:12 +0000

    converted to use saving of process to database method

--- _main.py
+++ _main.py
@@ -9,27 +9,27 @@ errors = 0
 
 def refresh_report(folder_dir):
 
-	print("Refreshing repo for: "+folder_dir)
+	u_print("Refreshing repo for: "+folder_dir)
 	errors = 0
 
 	try:
 		repo = git.Repo(folder_dir)
 		o = repo.remotes.origin
 		o.pull()
-		print("REFRESH COMPLETE!")
-		print("")
+		u_print("REFRESH COMPLETE!")
+		u_print("")
 	except:
 		errors += 1
-		print("REFRESH REPORT FAILED FOR: "+folder_dir)
-		print("")
+		u_print("REFRESH REPORT FAILED FOR: "+folder_dir)
+		u_print("")
 
 	return errors
 
-print('########################################')
-print("RUNNING GIT REFRESH PROCESS")
-print('########################################')
+u_print('########################################')
+u_print("RUNNING GIT REFRESH PROCESS")
+u_print('########################################')
 
-start_time = datetime.datetime.now() #need for process time printing
+start_time = datetime.datetime.now() #need for process time u_printing
 
 errors += refresh_report(base_path+'Email-Reader')
 errors += refresh_report(base_path+'Web-Service-Reader')
@@ -38,14 +38,16 @@ errors += refresh_report(base_path+'Service-Report')
 
 finish_time = datetime.datetime.now()
 
-print('')
-print('########################################')
-print("PROCESS COMPLETE")
-print("Number of Errors: "+str(errors))
-print('Start: '+str(start_time))
-print('End: '+str(finish_time))
-print('Time Taken: '+str(finish_time - start_time))
-print('########################################')
+u_print('')
+u_print('########################################')
+u_print("PROCESS COMPLETE")
+u_print("Number of Errors: "+str(errors))
+u_print('Start: '+str(start_time))
+u_print('End: '+str(finish_time))
+u_print('Time Taken: '+str(finish_time - start_time))
+u_print('########################################')
+
+save_process(start_time, finish_time, str(finish_time - start_time), "Git-Refresh", errors)
 
 #TESTS TO SEE IF THE SCHEDULER IS WORKING
 #file1 = open("test.txt","w")
