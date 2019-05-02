22b7ff0d020c69b21b186afea94bdb8b072c139b
SoapyD <tom_bombchild@hotmail.com>
2019-05-02 09:54:45 +0100

    removing email-reader as it's only a test project

--- _main.py
+++ _main.py
@@ -34,7 +34,7 @@ u_print('########################################')
 
 start_time = datetime.datetime.now() #need for process time u_printing
 
-errors += refresh_report(base_path+'Email-Reader')
+#errors += refresh_report(base_path+'Email-Reader') #NOT NEEDED ANYMORE, THIS IS A TEST EMAIL PROJECT
 errors += refresh_report(base_path+'Email-Attachment-Reader')
 errors += refresh_report(base_path+'Functions')
 errors += refresh_report(base_path+'Index-Rebuilder')
